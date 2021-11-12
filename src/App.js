18ff1f77393fd9bac8d09b9963211dd28f0a3e06
unknown <alihaiderizvi.you@gmail.com>
2021-11-12 21:39:40 +0500

    Button Toggle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import AddTask from './components/AddTask';
 import Header from './components/Header'; 
 import Tasks from './components/Tasks'; 
  function App() {  
+
+    const [showAddTask, setShowAddTask] = useState(false)
     const [tasks, setTasks] = useState([
         {
             id: 1, 
@@ -45,8 +47,8 @@ import Tasks from './components/Tasks';
 
      return (   
          <div className="container">
-             <Header />  
-             <AddTask onAdd={addTask}/>
+             <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>  
+             {showAddTask && <AddTask onAdd={addTask}/>}
              {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : <h3>No Task to show!</h3>}
          </div>
      );
