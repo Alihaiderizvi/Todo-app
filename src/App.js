e6f00b5c227cef5e2ad0bf6c0f5a95eadd1322f1
unknown <alihaiderizvi.you@gmail.com>
2021-11-15 13:15:41 +0500

    Add task to the server

--- src/App.js
+++ src/App.js
@@ -25,10 +25,21 @@ import Tasks from './components/Tasks';
 
     // Add Task 
 
-    const addTask = task => {
-        const id = Math.floor(Math.random() * 10000 +1 )
-        const newTask = {id, ...task}
-        setTasks([...tasks, newTask])
+    const addTask =async task => {
+        const res = await fetch("http://localhost:5000/tasks",{
+            method: 'POST',
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+
+        const data = await res.json()
+
+        setTasks([...tasks, data])
+        // const id = Math.floor(Math.random() * 10000 +1 )
+        // const newTask = {id, ...task}
+        // setTasks([...tasks, newTask])
     }
 
     //Delete Task 
@@ -37,7 +48,6 @@ import Tasks from './components/Tasks';
         await fetch(`http://localhost:5000/tasks/${id}`, {
             method : 'DELETE'
         })
-
         setTasks( tasks.filter ( task =>   task.id !== id))
     }
 
